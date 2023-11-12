c671d2ed24d1189343ef06058e301b170176d8cf
Kris Chambers <krischambers86@pm.me>
2023-11-12 14:00:32 -0500

    Finished day 2

--- 2015/day02iwastoldtherewouldbenomath.py
+++ 2015/day02iwastoldtherewouldbenomath.py
@@ -1,5 +1,6 @@
 # --- Day 2: I Was Told There Would Be No Math ---
 
+# Part 1
 # The elves are running low on wrapping paper, and so they need to submit an 
 # order for more. They have a list of the dimensions (length l, width w, and 
 # height h) of each present, and only want to order exactly as much as they 
@@ -13,23 +14,19 @@
 
 # For example:
 
-#    A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet 
-#    of wrapping paper plus 6 square feet of slack, for a total of 58 square 
-#    feet.
-#    A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square 
-#    feet of wrapping paper plus 1 square foot of slack, for a total of 43 
-#    square feet.
+# A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of 
+# wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+   
+# A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet 
+# of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
 
 # All numbers in the elves' list are in feet. How many total square feet of 
 # wrapping paper should they order?
 
 wrappingPaperSizes = []
-sortedWrappingPaperSizes = []
-
-
 inputFile = open("day02input.txt", "r")
 
-def createListFromFile():
+def createListFromFile():   
     for line in inputFile:
         newLine = line.splitlines()
         for line in newLine:   
@@ -39,19 +36,43 @@ def createListFromFile():
             wrappingPaperSizes.append(stringtoInt)
     inputFile.close()
 
-def doTheMath():
-    total = 0
+def doTheMath():   
+    totalWrappingPaper = 0
+    totalRibbon = 0
     for box in wrappingPaperSizes:
-        sides1 = 2*(box[0]*box[1])
-        sides2 = 2*(box[1]*box[2])
-        sides3 = 2*(box[0]*box[2])
+        side1 = 2*(box[0]*box[1])
+        side2 = 2*(box[1]*box[2])
+        side3 = 2*(box[0]*box[2])
         slack = box[0]*box[1]
-        total = total + sides1 + sides2 + sides3 + slack
-    print(f"The elves need to order {total} square feet of wrapping paper.")
+        totalWrappingPaper = totalWrappingPaper + side1 + side2 + side3 + slack
+        smallestPerimeter = 2*box[0] + 2*box[1]
+        cubicFeet = box[0]*box[1]*box[2]
+        totalRibbon = totalRibbon + smallestPerimeter + cubicFeet
+    print(f"The elves need to order {totalWrappingPaper} square feet of \
+wrapping paper and {totalRibbon} feet of ribbon.")
     
 
 createListFromFile()
 doTheMath()
 
+# Day 2 Part 2
+# The elves are also running low on ribbon. Ribbon is all the same width, so 
+# they only have to worry about the length they need to order, which they would 
+# again like to be exact.
 
+# The ribbon required to wrap a present is the shortest distance around its 
+# sides, or the smallest perimeter of any one face. Each present also requires 
+# a bow made out of ribbon as well; the feet of ribbon required for the perfect 
+# bow is equal to the cubic feet of volume of the present. Don't ask how they 
+# tie the bow, though; they'll never tell.
 
+# For example:
+# A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap 
+# the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 
+# feet.
+
+# A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap 
+# the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 
+# feet.
+
+# How many total feet of ribbon should they order?
