6030a094ca72c67032880655ff0b523084b66e3c
Kris <103455140+kris-chambers@users.noreply.github.com>
2023-12-01 16:53:55 -0500

    Refactor day 1 part 1

--- 2023/day01trebuchet.py
+++ 2023/day01trebuchet.py
@@ -1,13 +1,13 @@
 calibrationDocument = []
 listOfInts = []
-twoDigitList = []
 inputFile = open("2023\day01input.txt", "r")
+validDigits = ["one", "two", "three", "four", "five", "six", "seven", "eight"
+               "nine"]
 
-def createListFromFile():   
-    for line in inputFile:
-        calibrationDocument.append(line)
-    inputFile.close()
-#    print(calibrationDocument)
+def createListFromFile(listName, fileName):   
+    for line in fileName:
+        listName.append(line)
+    fileName.close()
     
 def removeNonInts():
     for i in calibrationDocument:
@@ -16,24 +16,20 @@ def removeNonInts():
             if character.isdigit():
                 ints.append(character)
         listOfInts.append(ints)
-#    print(listOfInts)
 
 def makeTwoDigitsAndSum():
     total = 0
     for i in listOfInts:
         if len(i) > 1:
             twoDigitNum = i[0] + i[-1]
-#            twoDigitList.append(int(twoDigitNum))
             total = total + int(twoDigitNum)
         else:
             twoDigitNum = i[0] + i[0]
-#            twoDigitList.append(int(twoDigitNum))
             total = total + int(twoDigitNum)
-#    print(twoDigitList)
     print(f"The sum of all of the calibration values is {total}.")       
             
                        
     
-createListFromFile()
+createListFromFile(calibrationDocument, inputFile)
 removeNonInts()
 makeTwoDigitsAndSum()
